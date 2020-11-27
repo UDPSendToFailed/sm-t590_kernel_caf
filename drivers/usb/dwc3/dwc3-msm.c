9fc02d84ac3ccf83e1ec81b1d8cf490a52643327
Rohith Kollalsi <rkollals@codeaurora.org>
2020-11-27 12:11:06 +0530

    usb: dwc3: Update return code before returning from probe
    Currently if failure to get dwc3 device occurs, probe exits
    but it returns 0 instead of error code. Platform driver
    assumes that probe is successful as it did not return any
    error code. Now if dwc3_msm_pm_suspend is called from
    platform_pm_suspend, dbg_event in dwc3_msm_pm_suspend accesses
    dwc3 which is NULL leading to NULL pointer de-reference.
    To fix this update return code before returning from probe.
    
    Change-Id: Id610c08ce97c162fe895b58373fc5734da159e6b
    Signed-off-by: Rohith Kollalsi <rkollals@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3421,6 +3421,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	of_node_put(dwc3_node);
 	if (!mdwc->dwc3) {
 		dev_err(&pdev->dev, "failed to get dwc3 platform device\n");
+		ret = -ENODEV;
 		goto put_dwc3;
 	}
 
@@ -3454,6 +3455,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	dwc = platform_get_drvdata(mdwc->dwc3);
 	if (!dwc) {
 		dev_err(&pdev->dev, "Failed to get dwc3 device\n");
+		ret = -ENODEV;
 		goto put_dwc3;
 	}
 #ifdef CONFIG_USB_CHARGING_EVENT
